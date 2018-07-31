09e5bfe8b72769fa9a8650fe130774279c79cdb7
cc <lezi2012@qq.com>
2018-07-31 17:22:32 +0800

    common dev

---
+++ compile.bat
@@ -0,0 +1,2 @@
+
+groovy ./compile_html.groovy
--- compile_html.groovy
+++ compile_html.groovy
@@ -2,22 +2,41 @@ def rootPath = new File(".").getCanonicalPath()
 
 def map = [:]
 
-new File("${rootPath}/widgets").eachFile { file ->
-    def fileName = file.getName()
-    map.put fileName, file.text
-}
+new File("${rootPath}/pages").eachFile { file ->
+    if (!file.isDirectory()) {
+        def fileName = file.getName()
+        if (fileName.startsWith("_"))
+            map.put fileName, file.text
+    }
 
+}
 
 new File("${rootPath}/pages").eachFile { file ->
-    def fileName = file.getName()
+    if (!file.isDirectory()) {
+        def fileName = file.getName()
 
-    def html = file.text
+        if (!fileName.startsWith("_")) {
+            def html = file.text
 
-    map.each { name, text ->
-        html = html.replaceAll(name, text)
+            map.each { name, text ->
+                html = html.replaceAll(name, text)
+            }
+            new File("${rootPath}/docs/${fileName}").newWriter().withWriter { w ->
+                w << html
+            }
+        }
     }
-    new File("${rootPath}/docs/${fileName}").newWriter().withWriter { w ->
-        w << html
-    }
-
 }
+
+
+
+String sourceDir = "${rootPath}/pages/res"
+String destinationDir = "${rootPath}/docs/res"
+new AntBuilder().copy(todir: destinationDir) {
+    fileset(dir: sourceDir)
+}
+
+
+
+
+
